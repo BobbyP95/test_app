d70de81d31c23e1c37310171faea9e985c1da34d
philips <philips18.bp@gmail.comgit config --listgit config --add --global user.email philips18.bp@gmail.com>
2025-03-27 18:35:33 +0000

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { AboutUs } from "./pages/AboutUs";
 import { Home } from "./pages/Home";
