7ae04e144ba8b7b377a1fe07fbf525424a3c8b3b
philips <philips18.bp@gmail.comgit config --listgit config --add --global user.email philips18.bp@gmail.com>
2025-07-17 23:03:30 +0000

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       dotfiles: true, // Include dotfiles (e.g., .nojekyll)
     }),
   ],
-  base: '/', // Correct for custom domains
+  base: '/test_app/', // Correct for custom domains
   build: {
     outDir: 'dist', // Ensure the build outputs to the 'dist' folder
     emptyOutDir: true, // Clear the output directory before building
